8ce3e7bce170fa85c1bdc183803aace13ef0d085
trungdd2811 <trungdd2811@gmail.com>
2024-07-01 14:31:32 +0700

    - fix SSL issue when connecting from http => https

--- 0.4 UIs/LearningAspire.Web/Program.cs
+++ 0.4 UIs/LearningAspire.Web/Program.cs
@@ -9,25 +9,31 @@ builder.AddRedisOutputCache(Constants.RedisCache);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+	.AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new($"https+http://{Constants.ApiService}");
-    });
+//builder.Services.AddHttpClient<WeatherApiClient>(client =>
+//    {
+//        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+//        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+//        client.BaseAddress = new($"https+http://{Constants.ApiService}");
+//    });
+
+var apiServiceUri = new Uri($"https+http://{Constants.ApiService}");
+builder.Services.AddHttpClient<WeatherApiClient>(client => client.BaseAddress = apiServiceUri);
+
+// Add a health-check for the weather API backend
+builder.Services.AddHealthChecks()
+	.AddUrlGroup(new Uri(apiServiceUri, "/health"), name: Constants.ApiService);
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+	app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
@@ -35,7 +41,7 @@ app.UseAntiforgery();
 app.UseOutputCache();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+	.AddInteractiveServerRenderMode();
 
 app.MapDefaultEndpoints();
 
